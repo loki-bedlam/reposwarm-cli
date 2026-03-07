edbd06edb8b9dce14a12fc0534d79721b9781b76
Loki <loki@meshmart.dev>
2026-03-07 23:48:52 +0000

    Revert "CRITICAL: Fix DynamoDB Local data persistence"
    This reverts commit 7fad499bcb8efc3f1f1f9fc777f0145f46ab9f82.

--- internal/bootstrap/local.go
+++ internal/bootstrap/local.go
@@ -658,7 +658,7 @@ services:
     image: amazon/dynamodb-local:latest
     ports:
       - "8000:8000"
-    command: ["-jar", "DynamoDBLocal.jar", "-sharedDb", "-dbPath", "/home/dynamodblocal/data"]
+    command: ["-jar", "DynamoDBLocal.jar", "-sharedDb"]
     volumes:
       - dynamodb-data:/home/dynamodblocal/data
 
